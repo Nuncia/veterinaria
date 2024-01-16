0529d3606112f710cb209c1895ec27f5a52318bc
Monica <maaylahuerta@gmail.com>
2024-01-16 14:03:42 -0300

    Se agrega validación a primer dato de los argumentos

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const {registrar, leer} = require('./operaciones');
 
 const argumentos = process.argv.slice(2);
-
-if(argumentos[0] === 'registrar'){
+const nombreFuncion = argumentos[0].toLowerCase();
+console.log(nombreFuncion);
+if(nombreFuncion === 'registrar'){
     registrar(argumentos[1], argumentos[2],argumentos[3], argumentos[4], argumentos[5])
-} else if(argumentos[0] === 'leer'){
+} else if(nombreFuncion === 'leer'){
     leer();
 } else{
     console.log('Debe ingresar la palabra registrar o leer.')
